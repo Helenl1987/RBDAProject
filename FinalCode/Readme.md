cdc357fce5a2c3f9446d8edeb595a956225f5fed
LaSummer <lizimo1216@gmail.com>
2018-12-11 19:44:07 -0500

    zl-final1

--- FinalCode/Readme.md
+++ FinalCode/Readme.md
@@ -1,48 +1,71 @@
 # Directories and Files
 * data_ingest
-	* code and commands for data ingestion
+  * code and commands for data ingestion
 * etl_code
-	* code for data cleaning
+  * code for data cleaning
 * profiling_code
-	* code for data profiling
+  * code for data profiling
 * code_iterations
-	* iterations
+  * iterations
 * screenshots
-	* screenshots that show the analytic running
+  * screenshots that show the combination model random forest and GBT spark jobs on three cities (Las Vegas, Toronto and Phoenix)
 * analytics
-	* analytics code
-	* geo_feature.py
-		* generate geographical features for businesses
-	* geo_jensen.py
-		* generate jensen feature for businesses
-	* mobile_feature.py
-		* generate mobility features for businesses
-	* lrtest_geo.py
-		* primary lr test to evaluate geographical features
-	* lrtest_mobile.py
-		* primary lr test to evaluate mobility features
-	* TrendSpliting.java
-		* generate time window for business trend data crawled from yelp
-		* build and run
-			* ```javac -classpath `yarn classpath` -d . TrendSpliting.java ```
-			* ```jar -cvf trendSpliting.jar *.class ```
-			* ```hadoop jar trendSpliting.jar TrendSpliting /user/jl10005/project/rating/crawl_result.tsv /user/jl10005/project/myout ```
-		* results location
-			* ```/user/jl10005/project/myout```
-	* DO.java
-		* review analysis (generate NLP features for reviews and do primary test to evaluate them)
-		* build and run
-			* ```javac -classpath /opt/cloudera/parcels/SPARK2-2.2.0.cloudera1-1.cdh5.12.0.p0.142354/lib/spark2/jars/*:`yarn classpath` DO.java ```
-  			* ```jar -cvf tryAnalytics.jar *.class```
-  			* ```spark2-submit --master yarn --deploy-mode cluster --class DO reviewAnalysis.jar ```
-  		* results location
-  			* may vary due to the invoking of different functions for various tasks, but all results will be under ```/user/jl10005/project/```
-  	* impala_record
-  		* primary table processing (selecting, filtering, joining) using impala
-  		* build and run
-  			* in impala shell
+  * analytics code
+  * geo_feature.py
+    * generate geographical features for businesses
+    * Build and run
+      *  run in yarn mode, change city name on line 14,17,108 can generate features on different cities and store in hdfs
+      * ```spark2-submit --master yarn --deploy-mode cluster --executor-memory 10G --num-executors 10 geofeature.py```
+  * mobile_feature.py
+    * generate mobility features for businesses
+    * Build and run
+      * run in local mode (can deploy to HDFS and yarn, but didn't do it, since data is relatively small)
+      * change checkin.txt file location on line 11, and features storage on 82 can generate features on different cities and store in local files
+  * geo_fit_trend_lrtest.py
+    * fit geo_features to business_trend to generate geo_trend_features and operate lrtest on geo_trend_features
+    * Build and run
+      * please run in pyspark (interactive mode)
+  * mobile_fit_trend_lrtest.py
+    * fit mobile_features to business_trend to generate mobile_trend_features and operate lrtest on mobile_trend_features
+    * Build and run
+      - please run in pyspark (interactive mode)
+  * TrendSpliting.java
+    * generate time window for business trend data crawled from yelp
+    * build and run
+    	* ```javac -classpath `yarn classpath` -d . TrendSpliting.java ```
+    	* ```jar -cvf trendSpliting.jar *.class ```
+    	* ```hadoop jar trendSpliting.jar TrendSpliting /user/jl10005/project/rating/crawl_result.tsv /user/jl10005/project/myout ```
+    * results location
+    	* ```/user/jl10005/project/myout```
+  * DO.java
+    * review analysis (generate NLP features for reviews and do primary test to evaluate them)
+    * build and run
+      * ```javac -classpath /opt/cloudera/parcels/SPARK2-2.2.0.cloudera1-1.cdh5.12.0.p0.142354/lib/spark2/jars/*:`yarn classpath` DO.java ```
+      	* ```jar -cvf tryAnalytics.jar *.class```
+      	* ```spark2-submit --master yarn --deploy-mode cluster --class DO reviewAnalysis.jar ```
+      * results location
+      	* may vary due to the invoking of different functions for various tasks, but all results will be under ```/user/jl10005/project/```
+    * impala_record
+      * primary table processing (selecting, filtering, joining) using impala
+      * build and run
+        * in impala shell
+  * rf_train.py
+    * random forest model trained  on combined features
+    * build and run:
+      * change city name on line 9,11,31can train and store model on different cities:
+      * ```spark2-submit --deploy-mode cluster --executor-memory 20G --conf spark.yarn.executor.memoryoverhead=4096M rf_train.py```
+  * gbt_train.py
+    * gradient-boosted tree model trained on combined features
+    * build and run:
+      * change city name on line 15,17,37 can train and store model on different cities
+      * ```spark2-submit --deploy-mode cluster --executor-memory 20G --conf spark.yarn.executor.memoryoverhead=4096M gbt_train.py```
+  * xgb_train.py
+    * extreme gradient-boosted tree model trained on combined features
+    * build and run
+      * please run in local intercative mode (python file)
+      * input format in libsvm
 * Readme.md
-	* readme file
+  * readme file
 
 
 # Input Data
--- FinalCode/analytics/geo_feature.py
+++ FinalCode/analytics/geo_feature.py
@@ -1,39 +1,30 @@
 import math
-import sys
-from pyspark.sql import Row
-from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from pyspark.sql import HiveContext
 from pyspark.sql import SQLContext
-
+import sys
+from pyspark.sql import Row
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-bfile = open('/Users/zimoli/Downloads/RBDA-MCINTOSH/Project/RBDAProject/spark_data/business_lv_30000.txt')
-
-lines = []
-
-for line in bfile:
-	lines.append(line[:-1].split('\t'))
-
-sc = SparkContext("local", "Density")
+sc = SparkContext("yarn", "Density")
 sqlContext = HiveContext(sc)
 
-rdd = sc.parallelize(lines)
-df = sqlContext.createDataFrame(rdd, ['name', 'neighborhood','business_id','main_cate','cate_count','longitude','latitude','cuisines','cuisine_count','dates'])
-df_d = df.na.drop()
+#change city name on line 14,17,108 than can iterate
+df_all_lv = sqlContext.sql("select business_all.name, business_all.neighborhood, business_all.business_id, business_all.main_cate, business_all.cate_count, business_all.longitude,business_all.latitude,business_all.cuisines,business_all.cuisine_count,business_open.dates from zl2521.business_all,zl2521.business_open where business_all.city='phoenix' and business_all.business_id=business_open.business_id")
+df_all_lv_d = df_all_lv.na.drop()
 
-df_d_f = df_d.filter((df_d.longitude != 'NULL') & (df_d.latitude != 'NULL'))
+df_res_lv = sqlContext.sql("select business_all.name, business_all.neighborhood, business_all.business_id, business_all.main_cate, business_all.cate_count, business_all.longitude,business_all.latitude,business_all.cuisines,business_all.cuisine_count,business_open.dates from zl2521.business_all,zl2521.business_open where business_all.city='phoenix' and business_all.business_id=business_open.business_id")
 
-all_list = sc.broadcast([[str(row['business_id']),str(row['name']),str(row['neighborhood']),str(row['main_cate']),str(row['latitude']),str(row['longitude']),str(row['dates']),str(row['cuisines']),str(row['cate_count']),str(row['cuisine_count'])] for row in df_d_f.collect()])
+df_res_lv_d = df_res_lv.na.drop()
+
+all_list = sc.broadcast([[str(row['business_id']),str(row['name']),str(row['neighborhood']),str(row['main_cate']),str(row['latitude']),str(row['longitude']),str(row['dates']),str(row['cuisines']),str(row['cate_count']),str(row['cuisine_count'])] for row in df_all_lv_d.collect()])
 
 cates = sc.broadcast(["restaurants", "shopping", "home services",  "financial services", "professional services", "hotels & travel", "active life", "health & medical", "arts & entertainment", "pets", "public services & government", "event planning & services", "education", "religious organizations", "automotive",  "local services", "beauty & spas", "real estate", "nightlife", "mass media", "local flavor", "food"])
 
 cuisines = sc.broadcast(["afghan","african","american (new)","american (traditional)","canadian (new)","canadian (traditional)","arabian","argentine","armenian","asian fusion","australian","austrian","bangladeshi","barbeque","basque","belgian","brasseries","brazilian","breakfast & brunch","british","buffets","burgers","burmese","cafes","cafeteria","cajun/creole","cambodian","caribbean","catalan","cheesesteaks","chicken shop","chicken wings","chinese","comfort food","creperies","cuban","czech","delis","diners","dinner theater","ethiopian","fast food","filipino","fish & chips","fondue","food court","food stands","french","game meat","gastropubs","german","gluten-free","greek","guamanian","halal","hawaiian","himalayan/nepalese","honduran","hong kong style cafe","hot dogs","hot pot","hungarian","iberian","indian","indonesian","irish","italian","japanese","kebab","korean","kosher","laotian","latin american","live/raw food","malaysian","mediterranean","mexican","middle eastern","modern european","mongolian","moroccan","new mexican cuisine","nicaraguan","noodles","pakistani","pan asia","persian/iranian","peruvian","pizza","polish","polynesian","pop-up restaurants","portuguese","poutineries","russian","salad","sandwiches","scandinavian","scottish","seafood","singaporean","slovakian","soul food","soup","southern","spanish","sri lankan","steakhouses","supper clubs","sushi bars","syrian","taiwanese","tapas bars","tapas/small plates","tex-mex","thai","turkish","ukrainian","uzbek","vegan","vegetarian","vietnamese","waffles","wraps"])
 
 timestamps = sc.broadcast(["2018_10", "2018_09", "2018_08","2018_07", "2018_06", "2018_05", "2018_04", "2018_03","2018_02", "2018_01", "2018_00","2017_11", "2017_10", "2017_09", "2017_08", "2017_07", "2017_06", "2017_05", "2017_04", "2017_03", "2017_02", "2017_01", "2017_00","2016_11", "2016_10", "2016_09", "2016_08", "2016_07", "2016_06", "2016_05", "2016_04", "2016_03", "2016_02", "2016_01", "2016_00","2015_11", "2015_10", "2015_09", "2015_08", "2015_07", "2015_06", "2015_05", "2015_04", "2015_03", "2015_02", "2015_01", "2015_00", "2014_11", "2014_10", "2014_09", "2014_08", "2014_07", "2014_06", "2014_05", "2014_04", "2014_03", "2014_02", "2014_01", "2014_00"])
-
-pt = df_d_f.rdd
 
 def distance(slat1, slon1, slat2, slon2):
     lat1 = float(slat1)
@@ -48,6 +39,7 @@ def distance(slat1, slon1, slat2, slon2):
          math.sin(dlon / 2) * math.sin(dlon / 2))
     return 2*math.atan2(math.sqrt(a), math.sqrt(1 - a))*radius
 
+#consider the cate count in the neighborhood
 def cate_count_ts(line):
     slat1 = line['latitude']
     slon1 = line['longitude']
@@ -82,7 +74,7 @@ def cate_count_ts(line):
                 if int(row[9]) != 0:
                     for tc in thiscuisines:
                         if tc in cuisine_dict:
-                            cuisine_dict[tc] += 1  
+                            cuisine_dict[tc] += 1
         ent = 0
         cates_dict = {}
         if all_count == 0:
@@ -110,9 +102,7 @@ def cate_count_ts(line):
         index -= 1
     return result
 
+
+pt = df_res_lv_d.rdd
 pt_cate_ts = pt.flatMap(lambda x: cate_count_ts(x))
-pt_cate_ts.saveAsPickleFile('/Users/zimoli/Downloads/RBDA-MCINTOSH/Project/RBDAProject/spark_data/geoprocess_lv_output_30000')
-
-
-
-
+pt_cate_ts.saveAsPickleFile("/user/zl2521/project/phoenix_cate_ts")
